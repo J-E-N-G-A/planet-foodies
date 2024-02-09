d71155203f5b024d5ce87339fb72f6a4ae35ca82
Eduardo Morales <emoral435@gmail.com>
2024-02-09 17:54:59 -0600

    chore: added NcVue and router libraries
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,80 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import createGlobe from 'cobe';
+import NcButton from '@nextcloud/vue'
+
+const el = ref();
+const phi = ref(0);
+
+onMounted(() => {
+  const globe = createGlobe(el.value, {
+    devicePixelRatio: 2,
+    width: 300 * 2,
+    height: 300 * 2,
+    phi: 0,
+    theta: 0,
+    dark: 1,
+    diffuse: 1.2,
+    mapSamples: 16000,
+    mapBrightness: 6,
+    baseColor: [0.3, 0.3, 0.3],
+    markerColor: [0.1, 0.8, 1],
+    glowColor: [1, 1, 1],
+    markers: [
+      // longitude latitude
+      { location: [41.8781, -87.6298], size: 0.3 },
+    ],
+    onRender: (state) => {
+      // Called on every animation frame.
+      // state will be an empty object, return updated params.
+      state.phi = phi.value;
+      phi.value += 0.01;
+    },
+  });
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <h1 id="Foodie"
+      class="Welcome">
+      Welcome to Planet Foodie
+      <div class="Welcome__enable">
+        At this planet, we want to be able to make sure you are able to eat out with friends.
+      </div>
+    </h1>
+    <canvas :style="{ width: '300px', height: '300px' }" ref="el"></canvas>
+    <h1>
+      <div>Planet Foodie</div>
+      <div>Planet Foodie</div>
+      <div>Planet Foodie</div>
+      <div>Planet Foodie</div>
+      <div>Planet Foodie</div>
+      <div>Planet Foodie</div>
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+html,
+body {
+  margin: 0;
+  background: black;
+  color: white;
+  font-family: sans-serif;
+  text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.app {
+  display: grid;
+  place-items: center;
+  place-content: center;
+  height: 100vh;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#Foodie {
+  relative: absolute;
+  top: 100px;
+  font-size: 60px;
 }
 </style>
