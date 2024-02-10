a0a56fff2d31ff312f386da4ecd9cdcc35c371ef
rapterjet2004 <juliuslinus1@gmail.com>
2024-02-10 00:17:58 -0600

    Isolated globe into it's own component
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

---
+++ frontend/src/components/Globe.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref, onMounted, ReactiveEffect } from 'vue';
+import createGlobe from 'cobe';
+
+const props = defineProps({
+  size: Number,
+  center: Boolean
+})
+
+var GLOBE_SIZE = props.size
+const el = ref();
+const phi = ref(0);
+
+onMounted(() => {
+  const globe = createGlobe(el.value, {
+    devicePixelRatio: 2,
+    width: GLOBE_SIZE*2,
+    height: GLOBE_SIZE*2,
+    phi: 0,
+    theta: 0.5,
+    dark: 0.8,
+    diffuse: 1.2,
+    mapSamples: 5000,
+    mapBrightness: 6,
+    baseColor: [0.6, 0.5, 0.7],
+    markerColor: [0.1, 0.8, 1],
+    glowColor: [.6, .5, 1],
+    markers: [
+      // longitude latitude
+      { location: [41.8781, -87.6298], size: 0.1 },
+    ],
+    onRender: (state) => {
+      // Called on every animation frame.
+      // state will be an empty object, return updated params.
+      state.phi = phi.value;
+      phi.value += 0.03;
+    },
+  });
+});
+
+</script>
+<template>
+	<canvas v-if="props.center" id="globeCentered" ref="el"></canvas>
+    <canvas v-if="!props.center" id="globeRight" ref="el"></canvas>
+</template>
+
+<style scoped>
+#globeRight {
+  position: absolute;
+  top:0%;
+  right:0%;
+  width: 1000px; /* GLOBE_SIZE */
+  height: 1000px; /* GLOBE_SIZE */
+  z-index: 0;
+}
+
+#globeCentered {
+  position: absolute;
+  top:20%;
+  width: 1000px; /* GLOBE_SIZE */
+  height: 1000px; /* GLOBE_SIZE */
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import createGlobe from 'cobe';
+import Globe from '../components/Globe.vue';
 import { VmButton, VmContainer } from 'vue3-material';
 
 import JuliusCat from "../assets/SpaceCatsJulius.png"
@@ -8,38 +7,6 @@ import EddyCat from "../assets/SpaceCatsEddy.png"
 import AniCat from "../assets/SpaceCatsAni.png"
 import GerardCat from "../assets/SpaceCatsGerard.png"
 import ShaneCat from "../assets/SpaceCatsShane.png"
-
-const GLOBE_SIZE = 1000
-const el = ref();
-const phi = ref(0);
-
-onMounted(() => {
-  const globe = createGlobe(el.value, {
-    devicePixelRatio: 2,
-    width: GLOBE_SIZE*2,
-    height: GLOBE_SIZE*2,
-    phi: 0,
-    theta: 0.5,
-    dark: 0.8,
-    diffuse: 1.2,
-    mapSamples: 5000,
-    mapBrightness: 6,
-    baseColor: [0.6, 0.5, 0.7],
-    markerColor: [0.1, 0.8, 1],
-    glowColor: [.6, .5, 1],
-    markers: [
-      // longitude latitude
-      { location: [41.8781, -87.6298], size: 0.1 },
-    ],
-    onRender: (state) => {
-      // Called on every animation frame.
-      // state will be an empty object, return updated params.
-      state.phi = phi.value;
-      phi.value += 0.01;
-    },
-  });
-});
-
 
 </script>
 <template>
@@ -51,7 +18,7 @@ onMounted(() => {
 			<p>At this planet, we want to be able to make sure you are able to eat out with friends. (now with space cats)</p>
 			<router-link to="/result">Want to learn more? Click here</router-link>
 		</div>
-		<canvas id="globe" ref="el"></canvas>
+		<Globe :size="1000" :center="true"></Globe>
 		<img id="cat-julius" :src="JuliusCat"/>
 		<img id="cat-eddy" :src="EddyCat"/>
 		<img id="cat-shane" :src="ShaneCat"/>
@@ -94,14 +61,6 @@ body {
   z-index: 1;
   font-family: Space;
 }
-
-#globe {
-  position: absolute;
-  top:20%;
-  width: 1000px; /* GLOBE_SIZE */
-  height: 1000px; /* GLOBE_SIZE */
-}
-
 
 #cat-julius {
   position: absolute;
