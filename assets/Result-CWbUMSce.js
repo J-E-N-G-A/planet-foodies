b0ca79c8b8137478ebe8114401a7577fd3ce6a9e
Eduardo Morales <emoral435@gmail.com>
2024-02-10 14:26:36 -0600

    Updates

---
+++ assets/Result-CWbUMSce.js
@@ -0,0 +1,1 @@
+import{G as e}from"./Globe-CWON0wFA.js";import{h as t,a,F as s,k as o,o as r}from"./index-BpHK8Ql5.js";const c=o("h1",null,"Result",-1),p={__name:"Result",setup(l){return(n,_)=>(r(),t(s,null,[c,a(e,{size:2300,center:!1})],64))}};export{p as default};
