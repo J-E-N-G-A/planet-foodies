5d2a860f296de461c73136ababb463c6878e3514
rapterjet2004 <juliuslinus1@gmail.com>
2024-02-10 14:41:19 -0600

    made the map
    Signed-off-by: rapterjet2004 <juliuslinus1@gmail.com>

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // views
 const routes = [
 	{ path: `/`, component: () => import('../views/Home.vue') },	
-	{ path: `/result`, component: import('../views/Result.vue') },	
+	{ path: `/result`, component: import('../views/Result.vue') },
+	{ path: `/maps`, component: import('../views/Resturant.vue') },
 ]
 
 const router = createRouter({
