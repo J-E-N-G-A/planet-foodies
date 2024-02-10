4ba56218eac9344178d038a4b55b16de25e4248f
Eduardo Morales <emoral435@gmail.com>
2024-02-10 08:12:07 -0600

    adding dist

--- src/views/PromptResponse.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-
-</script>
-<template>
-	<h1>Home YUHHHHHHH</h1>
-</template>
--- src/views/Result.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import Globe from '../components/Globe.vue';
-</script>
-<template>
-	<h1>Result</h1>
-	<Globe :size="2000" :center="false"></Globe>
-</template>
