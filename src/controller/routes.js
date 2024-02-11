b348ffb1acc50a1a3239765c46f95a2ffd51a983
Eduardo Morales <emoral435@gmail.com>
2024-02-11 08:37:01 -0600

    fix(cicd): error catching for chunkloading and re-loading
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -1,16 +1,33 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Result from '../views/Result.vue'
+import Map from '../views/Resturant.vue'
+import PromptResponse from '../views/PromptResponse.vue'
 
 // views
 const routes = [
-	{ path: `/`, component: () => import('../views/Home.vue') },	
-	{ path: `/result`, component: import('../views/Result.vue') },
-	{ path: `/maps`, component: import('../views/Resturant.vue') },
-	{ path: `/prompt-response`, component: import('../views/PromptResponse.vue') },
+	{ path: `/`, component: () => Home },	
+	{ path: `/result`, component: Result },
+	{ path: `/maps`, component: Map },
+	{ path: `/prompt-response`, component: PromptResponse },
 ]
 
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes,
+})
+
+router.onError((error, to) => {
+	if (
+		error.message.includes('Failed to fetch dynamically imported module') ||
+		error.message.includes('Importing a module script failed')
+	) {
+		if (!to?.fullPath) {
+		window.location.reload();
+		} else {
+		window.location = to.fullPath;
+		}
+	}
 })
 
 export default router
